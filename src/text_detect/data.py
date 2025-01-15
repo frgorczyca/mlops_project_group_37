daa2515ef6ac5458405ce88e32863db46d4e947e
Franciszek Gorczyca <fr.gorczyca@gmail.com>
2025-01-15 03:28:39 +0100

    add data preprocessing (#9)

--- src/text_detect/data.py
+++ src/text_detect/data.py
@@ -15,6 +15,9 @@ class DatasetManager(Dataset):
 
     def __len__(self) -> int:
         """Return the length of the dataset."""
+        with open(self.data_path, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            return sum(1 for _ in reader)
 
     def __getitem__(self, index: int):
         """Return a given sample from the dataset."""
@@ -33,35 +36,38 @@ class DatasetManager(Dataset):
             if item.is_file():
                 shutil.copy(item, output_folder / item.name)
 
-    def get_latest_version(self, output_folder: Path) -> None:
-        # Find the latest version
-        highest_number = -1
-        highest_file = None
-        for item in output_folder.iterdir():
-            if item.is_file():
-                try:
-                    number = int(item.stem.split('_')[-1])
-                    if number > highest_number:
-                        highest_number = number
-                        highest_file = item
-                except ValueError:
-                    continue
+        self.get_version(output_folder)
 
-        if highest_file:
-            print(f"File with the highest number in the name: {highest_file}")
+    def get_version(self, output_folder: Path, version: str = "latest") -> None:
+        if version == "latest":
+            highest_number = -1
+            highest_file = None
+            for item in output_folder.iterdir():
+                if item.is_file():
+                    try:
+                        number = int(item.stem.split('_')[-1])
+                        if number > highest_number:
+                            highest_number = number
+                            highest_file = item
+                    except ValueError:
+                        continue
+
+            if highest_file:
+                print(f"File with the highest number in the name: {highest_file}")
+            else:
+                print("No files found in the output folder")
+                return
+            
+            self.data_path = output_folder / highest_file.name
         else:
-            print("No files found in the output folder")
-            return
-        
-        self.data_path = output_folder / highest_file.name
+            self.data_path = output_folder / f"data_{version}.csv"
 
 
 def preprocess(raw_data_path: Path, output_folder: Path) -> None:
     print("Creating an instance of dataset")
     dataset = DatasetManager(raw_data_path)
     dataset.preprocess(output_folder)
-    dataset.get_latest_version(output_folder)
-    i = dataset[400]
+    dataset.get_version(output_folder)
 
 
 if __name__ == "__main__":
